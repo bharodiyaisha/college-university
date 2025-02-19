43de492ca3e977119d0d26729fcb41acc3d3ac11
ishabharodiya21 <ishabharodiya208@gmail.com>
2025-02-19 21:27:43 +0530

    Implemented home, about and campus section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 function App() {
   return (
-    <div className="App">
-     University
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+      </div>
     </div>
   );
 }
